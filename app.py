0693877c4aa1823fb0a0f20f578db2e874709fb5
ismael10palacios-eng <ismael10palacios@gmail.com>
2026-09-23 21:04:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("parametros")
 st.write("Elaborado por: Adrian Encalada")
 
 archivo=st.file_uploader("cargue su archivo")
+tabla= pd.readcsv(archivo)
+st.write(tabla)
