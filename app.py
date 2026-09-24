550d1847e2dc1a59ad4c600b208d275f13f18685
ismael10palacios-eng <ismael10palacios@gmail.com>
2026-09-23 21:02:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("parametros")
 st.write("Elaborado por: Adrian Encalada")
+
+archivo=st.file_uploader("cargue su archivo")
