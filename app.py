d2a4d086be8715598f16703da806592d3b4ea883
ismael10palacios-eng <ismael10palacios@gmail.com>
2026-09-23 21:13:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("parametros")
 st.write("Elaborado por: Adrian Encalada")
 
-archivo=st.file_uploader("cargue su archivo")
+archivo = st.file_uploader("Cargue su archivo")
 if archivo is not None:
-tabla=pd.read_csv(archivo)
+tabla = pd.read_csv(archivo)
 st.write(tabla)
