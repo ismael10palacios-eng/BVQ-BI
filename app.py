787b293a92a9da705bb564abe6f428b79c5c2ba8
ismael10palacios-eng <ismael10palacios@gmail.com>
2026-09-23 21:06:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ st.sidebar.title("parametros")
 st.write("Elaborado por: Adrian Encalada")
 
 archivo=st.file_uploader("cargue su archivo")
+if archivo is not None:
 tabla= pd.readcsv(archivo)
 st.write(tabla)
