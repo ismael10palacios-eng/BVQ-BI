48e8531d99ab20278bbe87295197eb7624458582
ismael10palacios-eng <ismael10palacios@gmail.com>
2026-09-23 21:08:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.write("Elaborado por: Adrian Encalada")
 
 archivo=st.file_uploader("cargue su archivo")
 if archivo is not None:
-tabla= pd.readcsv(archivo)
+tabla=pd.readcsv(archivo)
 st.write(tabla)
